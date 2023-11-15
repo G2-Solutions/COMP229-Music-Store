f416aa4c518c7802cee26e449f7bcd8612d9ee8e
zgawlak <114508304+zgawlak@users.noreply.github.com>
2023-11-15 17:23:07 -0500

    Add auth routes and controller

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const userCtrl = require('../controllers/user.controller');
-const router = express.Router()
+const router = express.Router();
+
 router.route('/api/users')
     .get(userCtrl.list)
-    .post(userCtrl.create)
+    .post(userCtrl.create);
+
 router.route('/api/users/:userId')
     .get(userCtrl.read)
     .put(userCtrl.update)
-    .delete(userCtrl.remove)
-router.param('userId', userCtrl.userByID)
-router.route('/api/users').post(userCtrl.create)
-router.route('/api/users').get(userCtrl.list)
-router.param('userId', userCtrl.userByID)
-router.route('/api/users/:userId').get(userCtrl.read)
-router.route('/api/users/:userId').put(userCtrl.update)
-router.route('/api/users/:userId').delete(userCtrl.remove)
+    .delete(userCtrl.remove);
+
+router.param('userId', userCtrl.userByID);
 
 module.exports = router;
