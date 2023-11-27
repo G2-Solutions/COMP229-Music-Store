63c11161b00c6a74decdcd908b3410233f4134bb
NolanGarcia <ngarciat@my.centennialcollege.ca>
2023-11-26 21:19:12 -0500

    Added the product routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const userRoutes = require('./server/routes/user.routes');
 const assetsRouter = require("./server/assets-router");
 const authRoutes = require('./server/routes/auth.routes');
-
+const productRoutes = require('./server/routes/product.routes');
 
 const app = express();
 app.use(express.json());
@@ -13,12 +13,15 @@ app.use(cors());
 
 app.use('/', authRoutes);
 app.use('/', userRoutes);
+app.use('/', productRoutes);
 
 app.use("/src", assetsRouter);
 app.use("/", express.static(path.join(__dirname, "music-store/public")));
 
 app.get("/*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
     res.sendFile(path.join(__dirname, "music-store/public", "index.html"));
+
 })
 
 mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2', {
