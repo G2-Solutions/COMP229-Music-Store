b8a27ee89dd898a1b79b85ded2abcc62e6c962cf
NolanGarcia <ngarciat@my.centennialcollege.ca>
2023-11-30 19:03:07 -0500

    Adding edit buttin to profile account, update fuctions not linked to button properly though

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { list } from './api-user.js';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import auth from '../auth/auth-helper.js';
+import { update } from './api-user.js';
 
 const Profile = () => {
   const [users, setProfile] = useState([]);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const jwt = auth.isAuthenticated();
   const navigate = useNavigate();
   const location = useLocation();
@@ -29,6 +34,44 @@ const Profile = () => {
       abortController.abort();
     };
   }, []);
+
+  const updateProfile = (userId, user) => {
+    console.log('Updating profile for user ID:', userId);
+    console.log('Updating user data:', user);
+  
+    const jwt = auth.isAuthenticated();
+    update({ userId }, { t: jwt.token }, user)
+      .then((data) => {
+        console.log('Update response:', data);
+        if (data && data.error) {
+          console.log(data.error);
+        } else {
+          console.log('Profile updated successfully:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error updating profile:', error);
+      });
+  };
+  
+  
+  const handleSaveChanges = () => {
+    const userId = users.length > 0 ? users[0]._id : '';
+    console.log('User ID:', userId);
+    const updatedUser = {
+      name: newName || users[0].name,
+      email: newEmail || users[0].email,
+      password: newPassword,
+    };
+    console.log('Update Profile Function Call:', updatedUser);
+  
+    updateProfile(userId, updatedUser);
+    setEditModalOpen(false);
+    setNewName('');
+    setNewEmail('');
+    setNewPassword('');
+  };
+  
 
   return (
     <div style={{ padding: '16px', margin: '40px', maxWidth: '600px', margin: '0 auto' }}>
@@ -74,10 +117,10 @@ const Profile = () => {
               <p style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '8px' }}>{item.name}</p>
               <p style={{ fontSize: '1rem', color: '#777' }}>{item.email}</p>
 
-              {/* Edit Profile button */}
               <button
+                onClick={() => setEditModalOpen(true)}
                 style={{
-                  marginTop: '20px',
+                  marginTop: '8px',
                   padding: '8px',
                   backgroundColor: '#333',
                   color: '#fff',
@@ -87,6 +130,35 @@ const Profile = () => {
               >
                 Edit Profile
               </button>
+
+              {editModalOpen && (
+                <div
+                  style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    background: 'rgba(0, 0, 0, 0.5)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <div style={{ background: '#fff', padding: '16px', borderRadius: '8px', width: '300px' }}>
+                    <h2>Edit Profile</h2>
+                    <label>New Name:</label>
+                    <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+                    <label>New Email:</label>
+                    <input type="text" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+                    <label>Confirm Password:</label>
+                    <input type="password" />
+                    <button onClick={() => { console.log('Save Changes button clicked'); handleSaveChanges(); }}>
+                      Save Changes</button>
+                    <button onClick={() => setEditModalOpen(false)}>Cancel</button>
+                  </div>
+                </div>
+              )}
             </div>
           </li>
         ))}
