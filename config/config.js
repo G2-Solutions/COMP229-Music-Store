60eaf2f5512a962426857e50bf9f699c5c170f06
NolanGarcia <ngarciat@my.centennialcollege.ca>
2023-12-02 19:19:02 -0500

    zoe-branch merge

--- config/config.js
+++ config/config.js
@@ -3,11 +3,7 @@ import dotenv from 'dotenv'
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-<<<<<<< HEAD
-  jwtSecret: "YOUR_secret_key",
-=======
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
->>>>>>> 4b696a7ccd9cbe09c454a8333908645fc1817722
   mongoUri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2" ||
   process.env.MONGO_HOST ||
   'mongodb://' + (process.env.IP || 'localhost') + ':' +
